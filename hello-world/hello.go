c995c5c3cad5bbe57f5380b941134e461f6cb4f9
jm <30784663+joem04@users.noreply.github.com>
2026-09-12 19:24:59 +0100

    Added switch case for different langauges, also added tests for these cases.

--- hello-world/hello.go
+++ hello-world/hello.go
@@ -2,10 +2,30 @@ package main
 
 import "fmt"
 
-func Hello(name string) string {
-	return "Hello, " + name
+const spanish = "Spanish"
+const french = "French"
+const englishHelloPrefix = "Hello, "
+const spanishHelloPrefix = "Hola, "
+const frenchHelloPrefix = "Bonjour, "
+
+func Hello(name string, language string) string {
+
+	if name == "" { // default name to "World"
+		name = "World"
+	}
+
+	prefix := englishHelloPrefix // := declares and assigns, whereas = is just for assigning already declared
+
+	switch language { // switch case for langauges
+	case spanish:
+		return spanishHelloPrefix + name
+	case french:
+		return frenchHelloPrefix + name
+	}
+
+	return prefix + name
 }
 
 func main() {
-	fmt.Println(Hello("Joe"))
+	fmt.Println(Hello("Napoleon", "French"))
 }
--- hello-world/hello_test.go
+++ hello-world/hello_test.go
@@ -4,14 +4,26 @@ import "testing"
 
 func TestHello(t *testing.T) {
 	t.Run("saying hello to people", func(t *testing.T) {
-		got := Hello("Joe")  // what we put into function
-		want := "Hello, Joe" // expected output
+		got := Hello("Joe", "English") // what we put into function
+		want := "Hello, Joe"           // expected output
 		assertCorrectMessage(t, got, want)
 	})
 
 	t.Run("say 'Hello, World' when an empty string is supplied", func(t *testing.T) {
-		got := Hello("")
-		want := "Hello, World"
+		got := Hello("", "")
+		want := "Hello, World" // empty name defaults to "World", empty language defaults to english
+		assertCorrectMessage(t, got, want)
+	})
+
+	t.Run("in Spanish", func(t *testing.T) {
+		got := Hello("Jose", "Spanish")
+		want := "Hola, Jose"
+		assertCorrectMessage(t, got, want)
+	})
+
+	t.Run("in French", func(t *testing.T) {
+		got := Hello("Mathius", "French")
+		want := "Bonjour, Mathius"
 		assertCorrectMessage(t, got, want)
 	})
 }
